b65116a763e4dd0e75140d0b1c98fed06e324475
Jon Su <jon@wisesync.ai>
2024-08-07 10:12:15 +0800

    Fix compile errors with clang compiler

--- src/include/robin_hood.h
+++ src/include/robin_hood.h
@@ -206,10 +206,10 @@ static Counts& counts() {
 
 // workaround missing "is_trivially_copyable" in g++ < 5.0
 // See https://stackoverflow.com/a/31798726/48181
-#if defined(__GNUC__) && __GNUC__ < 5
-#    define ROBIN_HOOD_IS_TRIVIALLY_COPYABLE(...) __has_trivial_copy(__VA_ARGS__)
-#else
+#if defined(__clang__) || (defined(__GNUC__) && __GNUC__ >= 5)
 #    define ROBIN_HOOD_IS_TRIVIALLY_COPYABLE(...) std::is_trivially_copyable<__VA_ARGS__>::value
+#else
+#    define ROBIN_HOOD_IS_TRIVIALLY_COPYABLE(...) __has_trivial_copy(__VA_ARGS__)
 #endif
 
 // helpers for C++ versions, see https://gcc.gnu.org/onlinedocs/cpp/Standard-Predefined-Macros.html
